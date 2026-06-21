3ca2332cfdc79c47a8ccfc9b17a74d8e4337b974
Dragnia3008 <truongkhoa2006nguyen@gmail.com>
2026-06-21 19:16:54 +0700

    Update UserRepository.cs

--- backend/TuneVault.Infrastructure/Repositories/UserRepository.cs
+++ backend/TuneVault.Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,6 @@
-using System.Data;
-using Dapper;
 using TuneVault.Application.Interfaces;
-using TuneVault.Domain.Entities;
 
+namespace TuneVault.Infrastructure.Repositories;
 namespace TuneVault.Infrastructure.Repositories;
 
 public class UserRepository : IUserRepository
