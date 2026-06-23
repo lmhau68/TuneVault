e54551eea0e7f08e0112dde9c8a3593c1ab2a4c5
Dragnia3008 <truongkhoa2006nguyen@gmail.com>
2026-06-23 21:31:31 +0700

    Delete backend/TuneVault.Application/Interfaces/IAuthService.cs

--- backend/TuneVault.Application/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using TuneVault.Application.DTOs.Auth;
-
-namespace TuneVault.Application.Interfaces;
-
-public interface IAuthService
-{
-    Task<AuthResultDto> RegisterAsync(RegisterRequestDTO request);
-    Task<AuthResultDto> LoginAsync(LoginRequestDTO request);
-}
