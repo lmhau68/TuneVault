4f41644651d89d165666e13219c888153c685884
HauLai <haulaiminh7a1190106.qtr@gmail.com>
2026-06-27 22:27:40 +0700

    Them mo ta va thumbnail vào uploadmediadto

--- backend/TuneVault.Application/DTOs/Media/UploadMediaRequest.cs
+++ backend/TuneVault.Application/DTOs/Media/UploadMediaRequest.cs
@@ -6,13 +6,11 @@ namespace TuneVault.Application.DTOs.Media
     {
 
         public string Title { get; set; } = string.Empty;
-
         public string? Artist { get; set; }
-
         public string? Genre { get; set; }
-
         public string? Album { get; set; }
-
+        public string? Description { get; set; }
+        public IFormFile? Thumbnail { get; set; }
         public IFormFile File { get; set; } = default!;
     }
 }
